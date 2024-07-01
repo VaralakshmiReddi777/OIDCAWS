a14f98aa1a4c24d3169c8ffbb768cb03adf1fcac
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-01 12:46:38 +0200

    Update quicksight.tf

--- quicksight.tf
+++ quicksight.tf
@@ -1,7 +1,6 @@
 resource "aws_quicksight_user" "example" {
   session_name  = "an-author"
   email         = "author@example.com"
-  namespace     = "foo"
   identity_type = "IAM"
   iam_arn       = "arn:aws:iam::905418071784:user/testuser"
   user_role     = "AUTHOR"
