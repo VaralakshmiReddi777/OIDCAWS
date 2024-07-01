6ea3810c9df672f4eeb89ba354185c51ffddf639
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-01 11:03:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_ssm_parameter" "fsystem01" {
   name      = "/systems/fsystem01/config"
   type      = "String"
-  value     = "https://www.website2.com"
+  value     = "https://www.website3.com"
   overwrite = true
 }
 
 resource "aws_ssm_parameter" "fsystem02" {
   name            = "/systems/fsystem02/config"
   type            = "String"
-  value           = "https://www.ESL2.com"
+  value           = "https://www.ESL3.com"
   overwrite = true
 }
