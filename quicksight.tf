40cae2eb38dc272f07cfe7b1c15b9bdcda27e729
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-01 16:05:15 +0200

    Delete quicksight.tf

--- quicksight.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_quicksight_user" "example" {
-  session_name  = "an-author"
-  email         = "author@example.com"
-  identity_type = "IAM"
-  iam_arn       = "arn:aws:iam::905418071784:user/testuser"
-  user_role     = "AUTHOR"
-}
-
