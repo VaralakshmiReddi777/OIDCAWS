882cd7a86b33a8228fadf8642cf998d747361bce
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-02 13:47:43 +0200

    Update and rename iam.tf-bkp to iam.tf

--- iam.tf
+++ iam.tf
@@ -49,3 +49,29 @@ resource "aws_iam_policy_attachment" "cloudwatch_logs_attachment" {
   policy_arn = aws_iam_policy.cloudwatch_logs_policy.arn
 }
 
+# Grant permission to write data to DynamoDB
+resource "aws_iam_policy" "dynamodb_write_policy" {
+  name        = "dynamodb-write-policy"
+  description = "Policy to allow writing to DynamoDB"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+        ],
+        Effect   = "Allow",
+        Resource = aws_dynamodb_table.realtime-data-table.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "dynamodb_write_policy_attachment" {
+  name       = "dynamodb_write_policy_attachment"
+  roles      = [aws_iam_role.lambda_execution_role.name]
+  policy_arn = aws_iam_policy.dynamodb_write_policy.arn
+}
