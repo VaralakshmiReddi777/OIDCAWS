83c1e0f796debdc5f99c830df5579a470f8e0dca
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 13:36:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
-# Create a CloudWatch Events rule to trigger the Lambda function on every `terraform apply`
-resource "aws_cloudwatch_event_rule" "my_lambda_trigger" {
-  name        = "my-lambda-trigger"
-  description = "Trigger Lambda on Terraform apply"
-  event_pattern = jsonencode({
-    source      = ["aws.terraform"],
-    detail_type = ["Terraform Apply"]
-  })
+resource "aws_ses_email_identity" "source_alert_email" {
+  email = "varubasie@gmail.com"
 }
 
-# Create a CloudWatch Events target to invoke the Lambda function
-resource "aws_cloudwatch_event_target" "my_lambda_target" {
-  rule      = aws_cloudwatch_event_rule.my_lambda_trigger.name
-  arn       = "arn:aws:lambda:us-east-2:905418071784:function:Spacelift_Test_Lambda_Function"
-  target_id = "my-lambda-target"
+resource "aws_ses_email_identity" "delivery_alert_email" {
+  email = "reddilohith7@gmail.com"
 }
