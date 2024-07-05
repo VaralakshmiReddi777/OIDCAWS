4636fd22b3513a8cc55c8dbbc8f8f6318f98d456
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 13:55:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,4 @@
-provider "aws" {
-  region = "us-east-2"
+resource "aws_iam_role_policy_attachment" "shopFloorAlert_lambda_role_attach" {
+  role       = aws_iam_role.shopFloorAlert_lambda_role.name
+  policy_arn = aws_iam_policy.shopFloorAlert_lambda_policy.arn
 }
-
-resource "aws_iam_role" "shopFloorAlert_lambda_role" {
-  name = "shopFloorAlert_lambda_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
