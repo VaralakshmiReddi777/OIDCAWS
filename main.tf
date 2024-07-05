bd6572728e4769faafe0167d533841a795c3f5c5
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 13:41:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,26 @@
-resource "aws_ses_email_identity" "source_alert_email" {
-  email = "varubasi@gmail.com"
-}
+resource "aws_iam_policy" "shopFloorAlert_lambda_policy" {
+  name        = "shopFloorAlert_lambda_policy"
+  path        = "/"
+  description = "Policy to be attached to lambda"
 
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ses:*",
+          "logs:*",
+          "dynamodb:DescribeStream",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:ListStreams",
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
