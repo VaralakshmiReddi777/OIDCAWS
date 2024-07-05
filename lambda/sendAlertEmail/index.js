c569fa6b525a40a3bdd0368c30406a22f93e0088
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 14:13:28 +0200

    Update index.js

--- lambda/sendAlertEmail/index.js
+++ lambda/sendAlertEmail/index.js
@@ -51,21 +51,3 @@ exports.handler = async (event) => {
         }
     }
 }
-
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "${path.module}/lambda/sendAlertEmail/index.js"
-  output_path = "sendAlertEmail.zip"
-}
-
-resource "aws_lambda_function" "send_alert_email" {
-  function_name = "SendAlertEmail"
-  role          = aws_iam_role.shopFloorAlert_lambda_role.arn
-  runtime       = "nodejs16.x"
-  filename      = "sendAlertEmail.zip"
-  handler       = "index.handler"
-  timeout       = "15"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-}
