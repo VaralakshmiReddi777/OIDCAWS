bdf439c6ba0599f92c1de5df7b79f757c28b7cf3
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 14:23:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_event_source_mapping" "trigger" {
   batch_size        = 100
-  event_source_arn  = "arn:aws:dynamodb:us-east-2:905418071784:table/shop_floor_alerts"
+  event_source_arn  = "arn:aws:dynamodb:us-east-2:905418071784:table/shop_floor_alerts/stream/2024-07-05T12:09:26.955"
   function_name     = "arn:aws:lambda:us-east-2:905418071784:function:SendAlertEmail"
   starting_position = "LATEST"
 }
