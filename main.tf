e916aae23dbfc3aa63be86d7de54a2380fa14a9d
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 13:43:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,24 @@
-resource "aws_iam_policy" "shopFloorAlert_lambda_policy" {
-  name        = "shopFloorAlert_lambda_policy"
-  path        = "/"
-  description = "Policy to be attached to lambda"
+resource "aws_iam_role" "shopFloorAlert_lambda_role" {
+  name = "shopFloorAlert_lambda_role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "VisualEditor0",
-        "Effect" : "Allow",
-        "Action" : [
-          "ses:*",
-          "logs:*",
-          "dynamodb:DescribeStream",
-          "dynamodb:GetRecords",
-          "dynamodb:GetShardIterator",
-          "dynamodb:ListStreams",
-        ],
-        "Resource" : "*"
-      }
-    ]
-  })
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "shopFloorAlert_lambda_role_attach" {
+  role       = aws_iam_role.shopFloorAlert_lambda_role.name
+  policy_arn = aws_iam_policy.shopFloorAlert_lambda_policy.arn
 }
