a26f3dfcfa828c8fbc56223fe67000aa33dda3d9
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 13:53:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,3 @@ resource "aws_iam_role" "shopFloorAlert_lambda_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "shopFloorAlert_lambda_role_attach" {
-  role       = aws_iam_role.shopFloorAlert_lambda_role.name
-  policy_arn = aws_iam_policy.shopFloorAlert_lambda_policy.arn
-}
