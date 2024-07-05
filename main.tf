a456963cffe34ae9e82d39cd31d63934af4309a4
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-07-05 13:40:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
 resource "aws_ses_email_identity" "source_alert_email" {
-  email = "varubasie@gmail.com"
+  email = "varubasi@gmail.com"
 }
 
-resource "aws_ses_email_identity" "delivery_alert_email" {
-  email = "reddilohith7@gmail.com"
-}
